3f6da4be39c19def4ec82a42958f58711dc95ccf
Zubair Qazi <zubair.qazi.5b@gmail.com>
2020-09-25 17:49:10 -0700

    Add Home, About, Contact pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import {BrowserRouter as Router, /*Route,*/ Link } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
-class HeaderNav extends React.Component {
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+
+class Header extends React.Component {
   
   constructor(props) {
     super(props);
@@ -44,10 +48,13 @@ class HeaderNav extends React.Component {
               </Nav>
             </Navbar.Collapse>
           </Navbar>
+          <Route path='/' exact render={() => <Home title={this.state.home.title}/>} />
+          <Route path='/about' render={() => <About title={this.state.about.title}/>} />
+          <Route path='/contact' render={() => <Contact title={this.state.contact.title}/>} />
         </Container>
       </Router>
     );
   }
 }
 
-export default HeaderNav;
+export default Header;
