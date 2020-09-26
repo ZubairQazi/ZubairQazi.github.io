28267555598aab13398a31e90bb4510cb1b441f9
Zubair Qazi <zubair.qazi.5b@gmail.com>
2020-09-26 13:16:44 -0700

    Add projects page

---
+++ src/components/Projects.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Projects extends React.Component {
+
+  render() {
+    return (
+      <h1>
+        My Projects
+      </h1>
+    );
+  }
+}
+
+export default Projects;
