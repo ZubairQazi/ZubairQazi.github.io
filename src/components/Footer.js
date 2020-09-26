f6b04d4fd9e7294cd91dfd66a42cadec7beb2655
Zubair Qazi <zubair.qazi.5b@gmail.com>
2020-09-25 17:56:14 -0700

    Remove unused imports in Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import {BrowserRouter as Router, /*Route,*/ Link } from 'react-router-dom';
+import {BrowserRouter as Router/*, Route, Link*/} from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Nav from 'react-bootstrap/Nav';
 
 class Footer extends React.Component {
 
