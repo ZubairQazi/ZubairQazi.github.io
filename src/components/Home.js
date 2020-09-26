32a74f634f6a448c1e6e61b7bd7e83a2826b5ea0
Zubair Qazi <zubair.qazi.5b@gmail.com>
2020-09-26 14:45:13 -0700

    Update home page, project page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import Typist from 'react-typist';
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class Home extends React.Component {
 
   render() {
     return (
-      <h1>
-        <Typist>
-          <span> I'm Zubair! </span>
-          <Typist.Backspace count={12} delay={1000} />
-          <span> Check out my Portfolio! </span>
-        </Typist>
 
-      </h1>
+      <Jumbotron className="bg-transparent jumbotron-fluid p-0">
+        <Container fluid={true}>
+          <Row className='justify-content-center py-5'>
+            <Col md={10} sm={12}>
+              <h1 className='display-3 font-weight-bolder'>
+                <Typist cursor={{ hideWhenDone: true }}>
+                  <span> I'm Zubair! </span>
+                  <Typist.Backspace count={12} delay={1000} />
+                  <span> Check out my Portfolio! </span>
+                  <Typist.Backspace count={25} delay={1000} />
+                  <span> Zubair Qazi </span>
+                </Typist>
+              </h1>
+              <h3 className='display-5 font-weight-light'> Student @ UCR, Mobile Dev. + Data Science </h3>
+            </Col>
+          </Row>
+        </Container>
+      </Jumbotron>
     );
   }
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class Projects extends React.Component {
 
