6a979325d6921f9d7ee1f0929b21eb8259419f9e
Zubair Qazi <zubair.qazi.5b@gmail.com>
2020-09-26 17:59:10 -0700

    Update Contact page header

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,34 @@
 import React from 'react';
 
+import Typist from 'react-typist';
+
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
 class Contact extends React.Component {
 
   render() {
+
     return (
-      <h1>
-        Contact Me
-      </h1>
+      <div>
+        <Jumbotron className='bg-transparent jumbotron-fluid p-0'>
+          <Container fluid={true}>
+            <Row className='justify-content-center py-5'>
+              <Col md={10} sm={12}>
+                <h1 className='display-3 font-weight-bolder'>
+                  <Typist cursor={{ hideWhenDone: true }}>
+                    <span> Contact Me </span>
+                  </Typist>
+                </h1>
+              </Col>
+            </Row>
+          </Container>
+        </Jumbotron>
+      </div>
+
     );
   }
 }
