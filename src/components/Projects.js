1ff5efb3565216e5da63b7d798f3004225d95750
Zubair Qazi <zubair.qazi.5b@gmail.com>
2020-09-27 01:23:25 -0700

    Add placeholder cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,6 +6,9 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Tabs from 'react-bootstrap/Tabs'
+import Tab from 'react-bootstrap/Tab';
 
 class Projects extends React.Component {
 
@@ -26,6 +29,58 @@ class Projects extends React.Component {
             </Row>
           </Container>
         </Jumbotron>
+        <Container fluid={true}>
+          <Row className='justify-content-center py-5'>
+          <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+            <Tab eventKey="home" title="Home">
+              </Tab>
+            <Tab eventKey="profile" title="Profile">
+            </Tab>
+            <Tab eventKey="contact" title="Contact" disabled>
+            </Tab>
+          </Tabs>
+          </Row>
+        </Container>
+        <Container>
+          <Row className='justify-content-center'> 
+            <Col>
+            <Card style={{ width: '20rem' }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk of
+                  the card's content.
+                </Card.Text>
+              </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+            <Card style={{ width: '20rem' }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk of
+                  the card's content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: '20rem' }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk of
+                  the card's content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
       </div>
     );
   }
