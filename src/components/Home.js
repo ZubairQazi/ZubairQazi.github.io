4b332db1505974da3a430f8bfc0efdc670dd8858
Zubair Qazi <zubair.qazi.5b@gmail.com>
2020-09-26 17:49:38 -0700

    Update home page, add image folder

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,30 +4,64 @@ import Jumbotron from 'react-bootstrap/Jumbotron'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+
+import pfp from '../images/pfp.jpg';
+import gh from '../images/github.png';
+import li from '../images/linkedin.png';
 
 class Home extends React.Component {
 
   render() {
     return (
 
-      <Jumbotron className="bg-transparent jumbotron-fluid p-0">
+      <div>
+        <Jumbotron className='bg-transparent jumbotron-fluid p-0'>
+          <Container fluid={true}>
+            <Row className='justify-content-center py-5'>
+              <Col md={10} sm={12}>
+                <h1 className='display-3 font-weight-bolder'>
+                  <Typist cursor={{ hideWhenDone: true }}>
+                    <span> I'm Zubair! </span>
+                    <Typist.Backspace count={12} delay={1000} />
+                    <span> Check out my Portfolio! </span>
+                    <Typist.Backspace count={25} delay={1000} />
+                    <span> Zubair Qazi </span>
+                  </Typist>
+                </h1>
+                <h3 className='display-5 font-weight-light'> Student @ UCR, Mobile Dev. + Data Science </h3>
+              </Col>
+            </Row>
+          </Container>
+        </Jumbotron>
         <Container fluid={true}>
-          <Row className='justify-content-center py-5'>
+          <Row className='justify-content-center'>
             <Col md={10} sm={12}>
-              <h1 className='display-3 font-weight-bolder'>
-                <Typist cursor={{ hideWhenDone: true }}>
-                  <span> I'm Zubair! </span>
-                  <Typist.Backspace count={12} delay={1000} />
-                  <span> Check out my Portfolio! </span>
-                  <Typist.Backspace count={25} delay={1000} />
-                  <span> Zubair Qazi </span>
-                </Typist>
-              </h1>
-              <h3 className='display-5 font-weight-light'> Student @ UCR, Mobile Dev. + Data Science </h3>
+              <Image src={pfp} roundedCircle />
+            </Col>
+          </Row>
+          <Row className='justify-content-center'>
+            <h1 className='display-3 font-weight-bolder'> _________________ </h1>
+          </Row>
+        </Container>
+        <Jumbotron className='bg-transparent jumbotron-fluid p-0'>
+          <Container fluid={true}>
+            <Row className='justify-content-center py-5'>
+              <Col md={10} sm={12}>
+                <h4 className='display-6 font-weight-light'> Python | Kotlin | Java | Swift | C++ | HTML/CSS | Javascript</h4>
+              </Col>
+            </Row>
+          </Container>
+        </Jumbotron>
+        <Container fluid={true}>
+          <Row className='justify-content-center'> 
+            <Col xs={6} md={4}>
+              <a href='https://github.com/ZubairQazi' target='_blank'> <Image src={gh} rounded /> </a>
+              <a href='https://www.linkedin.com/in/zubair-qazi' target='_blank'> <Image src={li} rounded /> </a>
             </Col>
           </Row>
         </Container>
-      </Jumbotron>
+      </div>
     );
   }
 }
