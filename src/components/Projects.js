e8cfc5925be0bb23249d6833db50b55f8a82ba40
Zubair Qazi <zubair.qazi.5b@gmail.com>
2020-09-27 18:11:11 -0700

    Remove unused Card components

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,8 +6,6 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card';
-import CardDeck from 'react-bootstrap/CardDeck';
 import Tab from 'react-bootstrap/Tab';
 import Nav from 'react-bootstrap/Nav';
 
