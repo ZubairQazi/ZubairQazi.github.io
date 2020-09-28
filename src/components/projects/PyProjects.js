14297a87dca9f5d38c3220f08344e61ab5f7321c
Zubair Qazi <zubair.qazi.5b@gmail.com>
2020-09-27 18:08:12 -0700

    Remove unused components

--- src/components/projects/PyProjects.js
+++ src/components/projects/PyProjects.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import CardDeck from 'react-bootstrap/CardDeck';
 import Nav from 'react-bootstrap/Nav';
