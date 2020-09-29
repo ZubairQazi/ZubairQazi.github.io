2d477d81bab1e4b544e30df83748cf0763b8e41b
Zubair Qazi <zubair.qazi.5b@gmail.com>
2020-09-28 21:12:36 -0700

    Switch BrowserRouter with HashRouter

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import {HashRouter as Router, Route, Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
