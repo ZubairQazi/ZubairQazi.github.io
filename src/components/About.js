091367559e30bf14bbdda4be2cbd3c9d6ee499d9
Zubair Qazi <zubair.qazi.5b@gmail.com>
2020-09-28 17:39:24 -0700

    Update About page with information

--- src/components/About.js
+++ src/components/About.js
@@ -16,13 +16,38 @@ class About extends React.Component {
         <Jumbotron className='bg-transparent jumbotron-fluid p-0'>
           <Container fluid={true}>
             <Row className='justify-content-center py-5'>
-              <Col md={10} sm={12}>
+              <Col md={8} sm={12}>
                 <h1 className='display-3 font-weight-bolder'>
                   <Typist cursor={{ hideWhenDone: true }}>
                     <span> About Me </span>
                   </Typist>
                 </h1>
               </Col>
+            </Row>
+            <Row className='justify-content-center'> 
+              <Col md={6} sm={8}> 
+               <p> 
+                  I'm Zubair (pronounced Zoo-bear). I am currently a junior at 
+                  University of California, Riverside studying Computer Science. 
+                  I tend to focus on mobile application development and data science/analytics, 
+                  however I also enjoy dabbling in other concentrations that
+                  my friends and professors are willing to teach me about! A perfect 
+                  example is this website, which was built with some help from my friends,
+                  YouTube, and documentation!
+                </p>
+              </Col> 
+            </Row>
+            <Row className='justify-content-center'> 
+              <Col md={6} sm={8}>
+                <p>
+                  I also love Star Wars and LEGO. I have a collection of Star Wars 
+                  figures that I have on display at home. I also have a LEGO collection 
+                  of assorted sets, mostly ranging from Star Wars to Creator sets.
+                  Other than that, in my free time I like to read, play video games with 
+                  friends, or work on fun projects! I would like to be able to travel 
+                  around the world, but I'll need a job for that first. 😉
+                </p>
+              </Col> 
             </Row>
           </Container>
         </Jumbotron>
