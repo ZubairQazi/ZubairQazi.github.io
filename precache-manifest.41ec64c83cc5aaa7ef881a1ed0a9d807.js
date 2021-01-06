d7944b312f273fe0d2221e160a91fc0e8494ca4e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-01-06 00:06:47 +0000

    Deploy zubairqazi.com to github.com/ZubairQazi/ZubairQazi.github.io.git:gh-pages

---
+++ precache-manifest.41ec64c83cc5aaa7ef881a1ed0a9d807.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b767610ec1f021069e17072343847f6a",
+    "url": "/ZubairQazi.github.io/index.html"
+  },
+  {
+    "revision": "99e4e3712aa6a966dd34",
+    "url": "/ZubairQazi.github.io/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "f596663d3d344006c9c8",
+    "url": "/ZubairQazi.github.io/static/css/main.d0e96308.chunk.css"
+  },
+  {
+    "revision": "99e4e3712aa6a966dd34",
+    "url": "/ZubairQazi.github.io/static/js/2.0428f67e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ZubairQazi.github.io/static/js/2.0428f67e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f596663d3d344006c9c8",
+    "url": "/ZubairQazi.github.io/static/js/main.55311fa7.chunk.js"
+  },
+  {
+    "revision": "c17b9e675f4267dc2254",
+    "url": "/ZubairQazi.github.io/static/js/runtime-main.a9b3790c.js"
+  },
+  {
+    "revision": "238f507bd7fc47c53206d9b9cec49ea8",
+    "url": "/ZubairQazi.github.io/static/media/Zubairs_Resume_.238f507b.pdf"
+  },
+  {
+    "revision": "470bb174cb985d26cfeb8e949400c4be",
+    "url": "/ZubairQazi.github.io/static/media/android.470bb174.png"
+  },
+  {
+    "revision": "f83ce63a3bf00d03627d70600d0d85e6",
+    "url": "/ZubairQazi.github.io/static/media/dealectable.f83ce63a.png"
+  },
+  {
+    "revision": "42253a0dc3ffafda962f39ccc34f8cfd",
+    "url": "/ZubairQazi.github.io/static/media/linreg.42253a0d.png"
+  },
+  {
+    "revision": "680d3aa576d805cce069561f1c7a6280",
+    "url": "/ZubairQazi.github.io/static/media/pfp.680d3aa5.jpg"
+  },
+  {
+    "revision": "1ef745a3d53cfb1f2ab9c7babfdf3d36",
+    "url": "/ZubairQazi.github.io/static/media/portfolio.1ef745a3.png"
+  },
+  {
+    "revision": "f073c98660fc4290f4d3d8d8f7b1e046",
+    "url": "/ZubairQazi.github.io/static/media/python.f073c986.png"
+  },
+  {
+    "revision": "ba412ffdbe2507a62e8edbd61635016d",
+    "url": "/ZubairQazi.github.io/static/media/urdude.ba412ffd.png"
+  }
+]);
