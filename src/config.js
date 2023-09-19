225a32d1dcd9ff96dc5215ba26c22c0f5cdea058
Zubair Qazi <zubair.qazi.5b@gmail.com>
2023-09-19 01:21:34 -0700

    Update information and links

--- src/config.js
+++ src/config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  email: 'brittany.chiang@gmail.com',
+  email: 'zqazi004@ucr.edu',
 
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
+      url: 'https://github.com/ZubairQazi',
     },
     {
       name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
+      // url: 'https://www.instagram.com/bchiang7',
     },
     {
       name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
+      // url: 'https://twitter.com/bchiang7',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'https://www.linkedin.com/in/zubair-qazi',
     },
     {
       name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
+      // url: 'https://codepen.io/bchiang7',
     },
   ],
 
