4cbf73cdd3f88a948c411f08e81adab66693882c
Zubair Qazi <zubair.qazi.5b@gmail.com>
2023-09-23 11:30:12 -0700

    Update contact information

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -60,8 +60,7 @@ const Contact = () => {
       <h2 className="title">Get In Touch</h2>
 
       <p>
-        Although I’m not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I’ll try my best to get back to you!
+      Looking for a full-time role at a cool company where I can make a difference. If there's a match, let's chat!
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
