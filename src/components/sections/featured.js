739e632a59499c522bb87d68964bf38fe3e28428
Zubair Qazi <zubair.qazi.5b@gmail.com>
2023-09-27 00:03:43 -0700

    Update featured projects to include pneumonia detection

--- src/components/sections/featured.js
+++ src/components/sections/featured.js
@@ -322,7 +322,6 @@ const Featured = () => {
               tech
               github
               external
-              
             }
             html
           }
@@ -348,7 +347,7 @@ const Featured = () => {
   return (
     <section id="projects">
       <h2 className="numbered-heading" ref={revealTitle}>
-        Some Things I’ve Built
+        Featured Projects
       </h2>
 
       <StyledProjectsGrid>
