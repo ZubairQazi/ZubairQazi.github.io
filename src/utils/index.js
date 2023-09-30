053dd5d9b5c9c86ca2cc0b40508c9a3520bfefa5
Zubair Qazi <zubair.qazi.5b@gmail.com>
2023-09-30 02:01:35 -0700

    Add slight delay to loading animation

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,8 +3,8 @@ export const hex2rgba = (hex, alpha = 1) => {
   return `rgba(${r},${g},${b},${alpha})`;
 };
 
-export const navDelay = 800;
-export const loaderDelay = 800;
+export const navDelay = 850;
+export const loaderDelay = 850;
 
 export const KEY_CODES = {
   ARROW_LEFT: 'ArrowLeft',
