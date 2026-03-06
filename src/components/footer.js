2df95ccd1aa2a700f7e5e339b3281aed4c829b7c
Zubair Qazi <zubair.qazi.5b@gmail.com>
2026-03-06 11:59:43 -0800

    remove Brittany Chiang attribution from footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -104,23 +104,6 @@ const Footer = () => {
         </ul>
       </StyledSocialLinks>
 
-      <StyledCredit tabindex="-1">
-        <a href="https://github.com/bchiang7/v4">
-          <div>Based on Brittany Chiang's original design.</div>
-          {/* {githubInfo.stars && githubInfo.forks && (
-            <div className="github-stats">
-              <span>
-                <Icon name="Star" />
-                <span>{githubInfo.stars.toLocaleString()}</span>
-              </span>
-              <span>
-                <Icon name="Fork" />
-                <span>{githubInfo.forks.toLocaleString()}</span>
-              </span>
-            </div>
-          )} */}
-        </a>
-      </StyledCredit>
     </StyledFooter>
   );
 };
